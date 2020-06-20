5e46681d396a754d6e549911ac642ae61b8cd561
nguyenthienquan <nguyenthienquan1996@gmail.com>
2020-06-20 16:10:29 +0700

    bai tap

---
+++ index.js
@@ -0,0 +1,199 @@
+// console.log("bai3.a");
+
+// for (let x = 0; x < 7; x++)
+// {
+//     console.log(x);
+// }
+
+// console.log("bai3.b");
+
+// let x = prompt('nhap vao 1 so');
+// for (let y = 0; y < x; y++)
+// {
+//     console.log(y);
+// }
+
+// console.log("bai3.c");
+
+// let x = prompt('nhap vao 1 so');
+// for (let y = 3; y < x; y++)
+// {
+//     console.log(y);
+// }
+
+// console.log("bai3.d");
+
+// let c = prompt('nhap vao so be');
+// let n = prompt('nhap vao so lon');
+// for ( let x = c; x < n; x++)
+// {
+//     console.log(x);
+// }
+
+// console.log("bai3.e");
+
+// let c = Number(prompt('nhap vao so be c'));
+// let n = Number(prompt('nhap vao so lon n'));
+
+// for (let x = c; x < n; x += 3)
+// {
+//     console.log(x);
+// }
+
+// console.log("bai3.f");
+
+// let c = Number(prompt('nhap vao so be c'));
+// let n = Number(prompt('nhap vao so lon n'));
+// let s = Number(prompt('nhap vao so s'));
+// for ( let x = c; x < n; x += s)
+// {
+//     console.log(x);
+// }
+
+
+
+// console.log("bai4");
+
+// let x = 1;
+// let n =prompt('nhap vao 1 so');
+// {
+//     for ( i = 1 ; i <= n; i++)
+
+//     x*=i;
+//     alert('ket qua cua tinh giai thua la :' + " " + x);
+// }
+
+// console.log("bai5");
+
+// let x = prompt(' nhap so tuoi cua ban');
+// if (x <= 14 )
+// {
+//   alert('you are no old enough to view this content');
+// }
+// else if ( x > 14)
+//   {
+//   alert('Enjoy');
+//   }
+
+// console.log("bai6");
+
+// let x =prompt('nhap vao 1 so ');
+// if ( x <=4 )
+// {
+//     alert('nằm ở nửa dưới từ 0-9');
+// } else if ( x < 9)
+// {
+//     alert('nam o nua tren tu 0-9');
+// } else
+// {
+//      alert('khong hop le');
+// }
+
+// console.log("bai7");
+
+// let x =prompt('nhap vao so x');
+// let n =prompt('nhap vao so n');
+
+// let y = n / 2;
+// if ( x < y)
+// {
+//     alert('x be hon 1 nua cua n');
+// } else if ( x > y)
+// {
+//     alert('x lon hon 1 nua cua n');
+// } else {
+//     alert('x bang 1 nua cua n');
+// }
+
+// console.log("bai8");
+
+// let x = prompt('nhap vao 1 so');
+// if ( x % 2 == 0 )
+// {
+//     alert('la so chan');
+// } else {
+//     alert('la so le');
+// }
+
+// console.log("bai9.a");
+
+// let x = "L";
+// let y = "H";
+// for ( let i = 1; i <= 3; i++)
+// {
+//     console.log(x);
+// }
+// for ( let i = 1; i <= 3; i++)
+// {
+//     console.log(y);
+// }
+
+// 
+
+// console.log("bai9.b");
+
+// let x = "L";
+// let y = "H";
+// let number = prompt('nhap vao 1 so');
+// let z = (number/2) - ((number%2)/2);
+
+// for ( let i = 1; i <= number - z; i++)
+// {
+//     console.log(x);
+// }
+// for ( let i = 1; i <= z; i++)
+// {
+//     console.log(y);
+// }
+
+// console.log("bai9.c");
+
+// for ( let x = 0; x < 8/2; x++)
+// {
+//     console.log(0);
+//     console.log(1);
+// }
+
+// console.log("bai9.d");
+
+// let x = prompt('nhap vao 1 so');
+// if ( x % 2 == 0)
+// {
+//     for ( let i = 1; i <= x/2; i++)
+//     {
+//         console.log(0);
+//         console.log(1);
+//     }
+// } else {
+//     for ( let i = 1; i <= x/2; i++)
+//     {
+//         console.log(0);
+//         console.log(1);
+//     }
+//         console.log(0);
+// }
+
+
+// console.log("bai10");
+
+// let x =prompt('chieu cao cua ban la');
+// let y =prompt('can nang cua ban la');
+
+// chisoBMI = y / ( x * x );
+// alert('chi so BMI cua ban la' + " " + chisoBMI);
+// if ( chisoBMI < 16 )
+// {
+//     alert('ban bi thieu can nang');
+// } else if ( chisoBMI < 18.5 )
+// {
+//     alert('ban bi thieu can');
+// } else if ( chisoBMI < 25)
+// {
+//     alert('chi so cua ban binh thuong');
+// } else if ( chisoBMI < 30)
+// {
+//     alert('ban bi thua can');
+// } else {
+//     alert('ban bi beo phi');
+// }
+
